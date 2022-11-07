e64043879f77b50dc35d6e4db22bfbb9a7c1c86e
Fault1err <oalkozlova@gmail.com>
2022-11-08 02:37:58 +0300

    feat: update task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -11,7 +11,7 @@ function exitApp() {
     exit();
 };
 
-fs.writeFile(fileCreate, 'Please write something!', err => {
+fs.writeFile(fileCreate, 'Please write something here!', err => {
     if (err) {
         throw error;
     }
