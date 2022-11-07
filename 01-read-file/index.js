8ec366ca035641676f355e4719c0411fc0e5c78e
Fault1err <oalkozlova@gmail.com>
2022-11-07 18:59:46 +0300

    feat: add task 1 update

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-// console.log('Название файла: ', path.basename(__filename));
-// console.log('Название файла: ', path.dirname(__filename));
-// console.log(path.join(__dirname, '01-read-file', 'text.txt'));
+// Вариант с fs.readFile
+// const name = path.join(__dirname, 'text.txt');
+// fs.readFile(name, 'utf8', (error, data) => {
+//     if (error) {
+//         throw error;
+//     }
+//     console.log(data);
+// });
 
-const name = path.join(__dirname, 'text.txt');
-
-fs.readFile(name, 'utf8', (error, data) => {
-    if (error) {
-        throw error;
-    }
-    console.log(data);
-});
+const nameStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+let data = '';
+nameStream.on('data', chunk => data += chunk);
+nameStream.on('end', () => console.log('End', data));
+nameStream.on('error', error => console.log('Error', error.message));
