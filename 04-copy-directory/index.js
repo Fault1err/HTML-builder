a45f43a0b3b3aa12a4b375cc4482be4dbf7c4ffb
Fault1err <oalkozlova@gmail.com>
2022-11-09 00:35:41 +0300

    fix: update task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,7 +6,7 @@ const newFolder = path.join(__dirname, 'files-copy');
 const copiedFolder = path.join(__dirname, 'files');
 
 function toCopyFolder() {
-    fs.readdir(copiedFolder, {withFileTypes: true}, (err, items) => {
+        fs.readdir(copiedFolder, {withFileTypes: true}, (err, items) => {
      items.forEach(item => {
         const newFile = path.join(newFolder, item.name);
         const copiedFile = path.join(copiedFolder, item.name);
@@ -18,7 +18,15 @@ function toCopyFolder() {
 };
 
 fs.mkdir(newFolder, {recursive: true}, err => {
+    fs.readdir(newFolder, (err, files) => {
         if (err) throw err;
+        for (file of files) {
+            fs.unlink(path.join(newFolder, file), err => {
+                if (err) throw err;
+            });
+        }
+    });
     toCopyFolder();
     console.log('Copied folder is created!');
-    });
+});
+
