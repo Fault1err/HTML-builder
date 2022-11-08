17826f69a775b30d7c0916337a9059d506e219bc
Fault1err <oalkozlova@gmail.com>
2022-11-08 21:25:06 +0300

    feat: update task 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -13,5 +13,5 @@ const path = require('path');
 const nameStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 let data = '';
 nameStream.on('data', chunk => data += chunk);
-nameStream.on('end', () => console.log('End', data));
+nameStream.on('end', () => console.log('', data));
 nameStream.on('error', error => console.log('Error', error.message));
