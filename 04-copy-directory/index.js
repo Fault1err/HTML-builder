9e4bd91d09442f7509401b32e82f3e238dee1af3
Fault1err <oalkozlova@gmail.com>
2022-11-08 19:01:09 +0300

    feat: add task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const path = require('path');
+const fs = require('fs');
 
+
+const newFolder = path.join(__dirname, 'files-copy');
+const copiedFolder = path.join(__dirname, 'files');
+
+function toCopyFolder() {
+    fs.readdir(copiedFolder, {withFileTypes: true}, (err, items) => {
+     items.forEach(item => {
+        const newFile = path.join(newFolder, item.name);
+        const copiedFile = path.join(copiedFolder, item.name);
+        fs.copyFile(copiedFile, newFile, function(err) {
+            if (err) throw err;
+        });
+        // console.log(item.name); 
+    });
+})
+};
+
+fs.mkdir(newFolder, {recursive: true}, err => {
+        if (err) throw err;
+    toCopyFolder();
+    console.log('Copied folder is created!');
+    });
