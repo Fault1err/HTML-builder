64ef080152f83a5aa4724565b6872ace1624df47
Fault1err <oalkozlova@gmail.com>
2022-11-08 21:23:14 +0300

    feat: add task 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fs = require('fs');
+
+const stylesFolder = path.join(__dirname, 'styles');
+const fileBundle = path.join(__dirname, 'project-dist', 'bundle.css');
+
+
+fs.writeFile(fileBundle, '', (err) => {
+    if (err) throw err;
+});
+
+fs.readdir(stylesFolder, (err, items) => {
+        items.forEach(item => {
+            if (path.extname(item) == '.css') {
+            fs.readFile(path.join(stylesFolder, item), 'utf-8', (err, data) => {
+                fs.appendFile(fileBundle, data, err => {
+                    if (err) throw err;
+                });
+                if (err) throw err;
+            });
+        }
+        });
+        if (err) throw err;
+    });
